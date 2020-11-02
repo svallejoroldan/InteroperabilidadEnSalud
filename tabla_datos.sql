eb9fe0ac97f749321098ec53b25cf14fe9512f7f
Juanpablo000 <57875962+Juanpablo000@users.noreply.github.com>
2020-11-02 14:10:24 -0500

    Ajustes menores

--- tabla_datos.sql
+++ tabla_datos.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.5
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 02-11-2020 a las 19:01:31
--- Versión del servidor: 5.7.24
--- Versión de PHP: 7.2.19
+-- Tiempo de generación: 02-11-2020 a las 19:09:19
+-- Versión del servidor: 10.3.16-MariaDB
+-- Versión de PHP: 7.3.23
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `datos`
+-- Base de datos: `id14752472_datos`
 --
 
 -- --------------------------------------------------------
@@ -28,31 +29,20 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `tabla_datos` (
-  `serial` varchar(40) NOT NULL,
+  `id` int(11) NOT NULL,
+  `serial` int(11) NOT NULL,
   `pulso` int(11) NOT NULL,
   `oxigeno` int(11) NOT NULL,
   `fecha` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `tabla_datos`
 --
 
-INSERT INTO `tabla_datos` (`serial`, `pulso`, `oxigeno`, `fecha`) VALUES
-('1', 30, 40, '2020-10-01'),
-('11', 45, 87, '2020-12-02'),
-('111', 111, 111, '2011-11-11'),
-('19', 24, 47, '2020-12-12'),
-('2', 44, 57, '2011-11-03'),
-('222', 222, 222, '2020-12-12'),
-('3', 22, 17, '2001-12-12'),
-('33', 33, 33, '2020-03-03'),
-('5', 60, 70, '2020-10-10'),
-('55', 55, 65, '2010-10-10'),
-('7', 7, 7, '2007-07-07'),
-('8', 8, 8, '2008-08-08'),
-('9', 9, 9, '2009-09-09'),
-('90', 90, 90, '2020-01-01');
+INSERT INTO `tabla_datos` (`id`, `serial`, `pulso`, `oxigeno`, `fecha`) VALUES
+(1, 20, 20, 20, '2020-10-10'),
+(2, 40, 25, 211, '2020-11-11');
 
 --
 -- Índices para tablas volcadas
@@ -62,7 +52,17 @@ INSERT INTO `tabla_datos` (`serial`, `pulso`, `oxigeno`, `fecha`) VALUES
 -- Indices de la tabla `tabla_datos`
 --
 ALTER TABLE `tabla_datos`
-  ADD PRIMARY KEY (`serial`);
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tabla_datos`
+--
+ALTER TABLE `tabla_datos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
